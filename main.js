08a97df7791c409b08abd928d9a6a49ba3aa41e7
CHEOL HUN LEE <doulos2008@naver.cpm>
2020-07-03 09:25:33 +0200

    JS: navbar menu click

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 "use strict";
-
+// Transparent Navbar
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
@@ -12,8 +12,23 @@ const handleScroll = () => {
   }
 };
 
+// Scroll to section
+const navbarMenu = document.querySelector(".navbar__menu");
+
+const handleClick = (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  } else {
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: "smooth" });
+  }
+};
+
 const init = () => {
   document.addEventListener("scroll", handleScroll);
+  navbarMenu.addEventListener("click", handleClick);
 };
 
 init();
